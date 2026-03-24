9d7c4d02ae345b8d1fe0251d349922db248bf397
HeidyLuna250 <heidyhueteluna0508@gmail.com>
2026-03-23 22:56:40 -0600

    Integración

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-//import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Encabezado from "./assets/components/navegacion/Encabezado.jsx";
+
+import Inicio from "./assets/views/Inicio.jsx";
+import Categorias from "./assets/views/Categorias.jsx";
+import Catalogo from "./assets/views/Catalogo.jsx";
+import Productos from "./assets/views/Productos.jsx";
+import Login from "./assets/views/Login.jsx";
+import Pagina404 from "./assets/views/Pagina404.jsx";
+import RutaProtegida from "./assets/components/rutas/RutaProtegida.jsx";
+
+import "./App.css";
 
 const App = () => {
-
   return (
-    <>
-      <h1>Componente principal</h1>
-    </>
+    <Router>
+
+      <Encabezado />
+
+      <main className="margen-superior-main">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/" element={<RutaProtegida><Inicio /></RutaProtegida>} />
+          <Route path="/categorias" element={<RutaProtegida><Categorias /></RutaProtegida>} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/productos" element={<RutaProtegida><Productos /></RutaProtegida>} />
+
+          <Route path="*" element={<Pagina404 />} />
+        </Routes>
+      </main>
+
+    </Router>
   );
-}
+};
 
 export default App;
